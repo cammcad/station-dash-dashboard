dab4efeb7185e14190b999bdae19ddef693f83df
Cameron Frederick <cfrederick@perx.com>
2021-10-09 20:08:08 -1000

    Genesis

--- data/db.js
+++ data/db.js
@@ -1,161 +1,159 @@
 /* DB */
-const data = 
-[
+const data = [
   {
-		name: 'Glenwood',
-		open: '6:00',
-		close: '6:00',
-		days: 'Sunday/Tuesday/Friday',
-		address: "18-2463 Ala Mahina'ai"
+    name: "Glenwood",
+    open: "6:00",
+    close: "6:00",
+    days: "Sunday/Tuesday/Friday",
+    address: "18-2463 Ala Mahina'ai",
   },
   {
-		name: 'Hāwī (Ka‘auhuhu)',
-		open: '6:00',
-		close: '6:00',
-		days: 'Monday/Tuesday/Wednesday/Thursday/Friday/Saturday/Sunday',
-		address: "55-3595 Kaʻauhuhu Road"
+    name: "Hāwī (Ka‘auhuhu)",
+    open: "6:00",
+    close: "6:00",
+    days: "Monday/Tuesday/Wednesday/Thursday/Friday/Saturday/Sunday",
+    address: "55-3595 Kaʻauhuhu Road",
   },
   {
-		name: 'Hilo',
-		open: '6:30',
-		close: '5:00',
-		days: 'Monday/Tuesday/Wednesday/Thursday/Friday/Saturday/Sunday',
-		address: "1471 Ho‘olaulima Road"
+    name: "Hilo",
+    open: "6:30",
+    close: "5:00",
+    days: "Monday/Tuesday/Wednesday/Thursday/Friday/Saturday/Sunday",
+    address: "1471 Ho‘olaulima Road",
   },
   {
-		name: 'Honoka‘a',
-		open: '6:00',
-		close: '6:00',
-		days: 'Monday/Tuesday/Wednesday/Thursday/Friday/Saturday/Sunday',
-		address: "45-624 Plumeria Street"
+    name: "Honoka‘a",
+    open: "6:00",
+    close: "6:00",
+    days: "Monday/Tuesday/Wednesday/Thursday/Friday/Saturday/Sunday",
+    address: "45-624 Plumeria Street",
   },
   {
-		name: 'Honomū',
-		open: '6:00',
-		close: '6:00',
-		days: 'Monday/Thursday/Saturday',
-		address: "28-1844 Old Māmalahoa Highway"
+    name: "Honomū",
+    open: "6:00",
+    close: "6:00",
+    days: "Monday/Thursday/Saturday",
+    address: "28-1844 Old Māmalahoa Highway",
   },
   {
-		name: 'Kailua-Kona (Kealakehe)',
-		open: '6:00',
-		close: '6:00',
-		days: 'Monday/Tuesday/Wednesday/Thursday/Friday/Saturday/Sunday',
-		address: "74-598 Hale Māka‘i Place"
+    name: "Kailua-Kona (Kealakehe)",
+    open: "6:00",
+    close: "6:00",
+    days: "Monday/Tuesday/Wednesday/Thursday/Friday/Saturday/Sunday",
+    address: "74-598 Hale Māka‘i Place",
   },
   {
-		name: 'Kalapana',
-		open: '8:00',
-		close: '4:00',
-		days: 'Saturday/',
-		address: "74-598 Hale Māka‘i Place"
+    name: "Kalapana",
+    open: "8:00",
+    close: "4:00",
+    days: "Saturday/",
+    address: "74-598 Hale Māka‘i Place",
   },
   {
-		name: 'Kea‘au',
-		open: '6:00',
-		close: '6:00',
-		days: 'Monday/Tuesday/Wednesday/Thursday/Friday/Saturday/Sunday',
-		address: "16-921 Kea‘au-Pāhoa Road"
+    name: "Kea‘au",
+    open: "6:00",
+    close: "6:00",
+    days: "Monday/Tuesday/Wednesday/Thursday/Friday/Saturday/Sunday",
+    address: "16-921 Kea‘au-Pāhoa Road",
   },
   {
-		name: 'Keauhou',
-		open: '6:00',
-		close: '6:00',
-		days: 'Monday/Tuesday/Wednesday/Thursday/Friday/Saturday/Sunday',
-		address: "79-7098 Māmalahoa Highway"
+    name: "Keauhou",
+    open: "6:00",
+    close: "6:00",
+    days: "Monday/Tuesday/Wednesday/Thursday/Friday/Saturday/Sunday",
+    address: "79-7098 Māmalahoa Highway",
   },
   {
-		name: 'Ke‘ei',
-		open: '6:00',
-		close: '6:00',
-		days: 'Tuesday/Friday/Sunday',
-		address: "83-6180 Pu‘uhonua Road"
+    name: "Ke‘ei",
+    open: "6:00",
+    close: "6:00",
+    days: "Tuesday/Friday/Sunday",
+    address: "83-6180 Pu‘uhonua Road",
   },
   {
-		name: 'Laupāhoehoe',
-		open: '6:00',
-		close: '6:00',
-		days: 'Tuesday/Friday/Sunday',
-		address: "35-2061 Old Māmalahoa Highway"
+    name: "Laupāhoehoe",
+    open: "6:00",
+    close: "6:00",
+    days: "Tuesday/Friday/Sunday",
+    address: "35-2061 Old Māmalahoa Highway",
   },
   {
-		name: 'Miloli‘i',
-		open: '6:00',
-		close: '6:00',
-		days: 'Monday/Tuesday/Wednesday/Thursday/Friday/Saturday/Sunday',
-		address: "89-1217 Miloli‘i Road"
+    name: "Miloli‘i",
+    open: "6:00",
+    close: "6:00",
+    days: "Monday/Tuesday/Wednesday/Thursday/Friday/Saturday/Sunday",
+    address: "89-1217 Miloli‘i Road",
   },
   {
-		name: 'Ocean View',
-		open: '8:00',
-		close: '4:00',
-		days: 'Monday/Thursday/Saturday',
-		address: "92-9017 Māmalahoa Highway"
+    name: "Ocean View",
+    open: "8:00",
+    close: "4:00",
+    days: "Monday/Thursday/Saturday",
+    address: "92-9017 Māmalahoa Highway",
   },
   {
-		name: 'Pa‘auilo',
-		open: '6:00',
-		close: '6:00',
-		days: 'Monday/Thursday/Saturday',
-		address: "43-1615 Hauola Road"
+    name: "Pa‘auilo",
+    open: "6:00",
+    close: "6:00",
+    days: "Monday/Thursday/Saturday",
+    address: "43-1615 Hauola Road",
   },
   {
-		name: 'Pāhala',
-		open: '6:00',
-		close: '6:00',
-		days: 'Sunday/Tuesday/Friday',
-		address: "96-1250 Old Māmalahoa Highway"
+    name: "Pāhala",
+    open: "6:00",
+    close: "6:00",
+    days: "Sunday/Tuesday/Friday",
+    address: "96-1250 Old Māmalahoa Highway",
   },
   {
-		name: 'Pāhoa',
-		open: '6:00',
-		close: '6:00',
-		days: 'Monday/Tuesday/Wednesday/Thursday/Friday/Saturday/Sunday',
-		address: "15-1870 Cemetery Road"
+    name: "Pāhoa",
+    open: "6:00",
+    close: "6:00",
+    days: "Monday/Tuesday/Wednesday/Thursday/Friday/Saturday/Sunday",
+    address: "15-1870 Cemetery Road",
   },
   {
-		name: 'Pāpa‘ikou',
-		open: '6:00',
-		close: '6:00',
-		days: 'Sunday/Tuesday/Friday',
-		address: "27-215 Old Māmalahoa Highway"
+    name: "Pāpa‘ikou",
+    open: "6:00",
+    close: "6:00",
+    days: "Sunday/Tuesday/Friday",
+    address: "27-215 Old Māmalahoa Highway",
   },
   {
-		name: 'Puakō',
-		open: '6:00',
-		close: '6:00',
-		days: 'Sunday/Tuesday/Friday',
-		address: "66-1471 Puakō Beach Drive"
+    name: "Puakō",
+    open: "6:00",
+    close: "6:00",
+    days: "Sunday/Tuesday/Friday",
+    address: "66-1471 Puakō Beach Drive",
   },
   {
-		name: 'Volcano',
-		open: '6:00',
-		close: '6:00',
-		days: 'Monday/Thursday/Saturday',
-		address: "19-3438 Volcano Road"
+    name: "Volcano",
+    open: "6:00",
+    close: "6:00",
+    days: "Monday/Thursday/Saturday",
+    address: "19-3438 Volcano Road",
   },
   {
-		name: 'Waiea',
-		open: '6:00',
-		close: '6:00',
-		days: 'Monday/Thursday/Saturday',
-		address: "86-3895 Hawai‘i Belt Road"
+    name: "Waiea",
+    open: "6:00",
+    close: "6:00",
+    days: "Monday/Thursday/Saturday",
+    address: "86-3895 Hawai‘i Belt Road",
   },
   {
-		name: 'Waimea',
-		open: '6:00',
-		close: '6:00',
-		days: 'Monday/Tuesday/Wednesday/Thursday/Friday/Saturday/Sunday',
-		address: "66-1597 Kawaihae Road"
+    name: "Waimea",
+    open: "6:00",
+    close: "6:00",
+    days: "Monday/Tuesday/Wednesday/Thursday/Friday/Saturday/Sunday",
+    address: "66-1597 Kawaihae Road",
   },
   {
-		name: 'Wai‘ōhinu',
-		open: '6:00',
-		close: '6:00',
-		days: 'Monday/Thursday/Saturday',
-		address: "95-6155 Kaulia Road"
+    name: "Wai‘ōhinu",
+    open: "6:00",
+    close: "6:00",
+    days: "Monday/Thursday/Saturday",
+    address: "95-6155 Kaulia Road",
   },
+];
 
-]
-
-module.exports = {data}
+module.exports = { data };
--- src/core.js
+++ src/core.js
@@ -2,32 +2,29 @@
 
 /* Types */
 
-const None = x =>
-({
-	type: 'None',
-	contents: () => x,
-	chain: f => None(x),
-	map: f => None(x),
-	fold: (f,g) => f(x),
-	inspect: () => `None(${x})`
-})
+const None = (x) => ({
+  type: "None",
+  contents: () => x,
+  chain: (f) => None(x),
+  map: (f) => None(x),
+  fold: (f, g) => f(x),
+  inspect: () => `None(${x})`,
+});
 
-const Some = x =>
-({
-	type: 'Some',
-	contents: () => x,
-	chain: f => f(x),
-	map: f => Some(f(x)),
-	fold: (f,g) => g(x),
-	inspect: () => `Some(${x})`
-})
+const Some = (x) => ({
+  type: "Some",
+  contents: () => x,
+  chain: (f) => f(x),
+  map: (f) => Some(f(x)),
+  fold: (f, g) => g(x),
+  inspect: () => `Some(${x})`,
+});
 
 /* Functions */
 
-const compose = (f,g) => x => f(g(x))
-const fromNullable = x =>
-	x != null || undefined ? Some(x) : None(null)
-const id = x => x
-const isNone = x => x.type === 'None'
+const compose = (f, g) => (x) => f(g(x));
+const fromNullable = (x) => (x != null || undefined ? Some(x) : None(null));
+const id = (x) => x;
+const isNone = (x) => x.type === "None";
 
-module.exports = {Some,None,fromNullable,isNone,id,compose}
+module.exports = { Some, None, fromNullable, isNone, id, compose };
